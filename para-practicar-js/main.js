92734c10ed4384401d2720138a89f180f37ffd2b
LJorgeJLandaida <jorgelandaida114@gmail.com>
2024-10-25 17:06:19 -0300

    tes

---
+++ para-practicar-js/main.js
@@ -0,0 +1,46 @@
+// let persona = {
+    // nombre: "jorge",
+    // edad: 24
+// };
+
+// console.log(persona.nombre);
+// console.log(persona.edad);
+
+// PRACTICAS ///
+
+// let alimentos = [
+    // {primer: "manzana"},
+    // {segunda: "banana"},
+    // {tercera: "pera"},
+// ];
+
+// console.log(alimentos[0].primer)
+// console.log(alimentos[1].segunda)
+// console.log(alimentos[2].tercera) 
+
+// let lista = {
+    // nombre:"frutas",
+    // preferidas: ["manzana","banana","pera"]
+// };
+
+// console.log(lista.preferidas.join(","));
+
+
+// EJERCICIO 3 //
+
+let usuario = prompt(`ingrese su fruta numero1 favorita: `)
+let fruta1 = prompt(`ingrese su segunda fruta: `)
+let fruta2 = prompt(`ingrese su tercera fruta: `)
+
+let mensaje = usuario + ` ` +fruta1 + ` ` + fruta2 
+
+console.log(mensaje)
+
+alert(`aca mis frutas preferidas separadas por una coma`)
+
+let lista = {
+    nombre: "frutas",
+    preferidas: ["manzana","banana","pera"]
+}
+
+console.log(lista.preferidas.join(","));
